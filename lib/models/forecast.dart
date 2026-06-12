907e377c9ff443ae1d9f55a7b7466053cd56eee9
Sikander Atif <sikanderatif07@gmail.com>
2026-06-12 19:03:57 +0500

    Created forecast model class and implemented fetchForecast method.

---
+++ lib/models/forecast.dart
@@ -0,0 +1,40 @@
+class ForecastItem {
+  final num date, high, low;
+  final String condition;
+
+  const ForecastItem({
+    required this.date,
+    required this.high,
+    required this.low,
+    required this.condition,
+  });
+}
+
+class Forecast {
+  final List<ForecastItem> forecast;
+
+  const Forecast({required this.forecast});
+
+  factory Forecast.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'list': List items} => Forecast(
+        forecast: [
+          for (final item in items)
+            if (item case {
+              'dt': num date,
+              'main': {'temp_min': num low, 'temp_max': num high},
+              'weather': [{'main': String condition}, ...],
+            })
+              ForecastItem(
+                date: date,
+                high: high,
+                low: low,
+                condition: condition,
+              ),
+        ],
+      ),
+
+      _ => throw const FormatException('Failed to load forecast data.'),
+    };
+  }
+}
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,20 +1,50 @@
+import 'package:weather_app/constants/api_constants.dart';
 import 'package:weather_app/models/weather.dart';
+import 'package:weather_app/models/forecast.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
 
 class WeatherService {
-  Future<Weather> fetchWeather() async {
-    final response = await http.get(
-      Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=ff0ca65da1a45b1cae07a9b5e069e52f&units=metric',
-      ),
-      headers: {'Accept': 'application/json'},
-    );
+  Future<Weather> fetchWeather(String city) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=${ApiConstants.openWeatherMapKey}&units=metric',
+        ),
+        headers: {'Accept': 'application/json'},
+      );
 
-    if (response.statusCode == 200) {
-      return Weather.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw ServiceException('City not found, no internet', 5);
+      if (response.statusCode == 200) {
+        return Weather.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>,
+        );
+      } else {
+        throw ServiceException('City not found', 5);
+      }
+    } on SocketException catch (e) {
+      throw SocketException('No Internet Connection: $e');
+    }
+  }
+
+  Future<Forecast> fetchForecast(String city) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=${ApiConstants.openWeatherMapKey}&units=metric',
+        ),
+        headers: {'Accept': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        return Forecast.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>,
+        );
+      } else {
+        throw ServiceException('City not found', 5);
+      }
+    } on SocketException catch (e) {
+      throw SocketException('No Internet Connection: $e');
     }
   }
 }
